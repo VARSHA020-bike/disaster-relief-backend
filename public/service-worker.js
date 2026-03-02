5ddc6935f6e7488840fb478a81590b8e465578fc
VARSHA020-bike <varshaammuvarghese36@gmail.com>
2026-03-02 20:07:35 +0530

    Initial backend commit

---
+++ public/service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "relief-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
+
+// Install service worker
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch requests
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
